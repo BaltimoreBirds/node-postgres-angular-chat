c1074ddaf78ea1a349bc3d1c8e5bb85db7990cba
Michael Swanson <mikeswanson12@msn.com>
2015-06-10 11:46:21 -0400

    updates routes to reflect refactored folder structure

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./server/routes/index');
+var users = require('./server/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname,'client', 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './client', 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 var path = require('path');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/chat';
+var connectionString = require(path.join(__dirname, '../', '../', 'config'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../views','index.html'));
+  res.sendFile(path.join(__dirname,'../','../','client', 'views','index.html'));
 });
 
 
