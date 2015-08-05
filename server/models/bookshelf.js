574bc4587c7c9d2f7b98bdf687abd166c7873c3b
Michael Swanson <mikeswanson12@msn.com>
2015-08-05 19:46:56 -0400

    updates database connection settings

--- server/models/bookshelf.js
+++ server/models/bookshelf.js
@@ -1,10 +1,10 @@
 var knex = require('knex')({
     client: 'pg',
     connection: {
-        host     : 'ec2-204-236-226-63.compute-1.amazonaws.com',
-        user     : 'kvimdzxrahtabs',
-        password : '2zIgeT-Xrga02UMhubnquL5q2f',
-        database : 'd75kv4ska29qoq',
+        host     : 'ec2-54-83-55-214.compute-1.amazonaws.com',
+        user     : 'tpqopthjjzjbip',
+        password : 'cjcg3Kpfc-KVwNOwKgZu5j2k6l',
+        database : 'ddb75r044ashp4',
         charset  : 'utf8'
   }
 });
