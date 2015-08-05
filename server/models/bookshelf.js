dfb98131e91fc27d8ed4872836d59a04204343a9
Michael Swanson <mikeswanson12@msn.com>
2015-08-05 18:48:49 -0400

    update bookshelf/knex database connection settings

--- server/models/bookshelf.js
+++ server/models/bookshelf.js
@@ -1,11 +1,12 @@
 var knex = require('knex')({
     client: 'pg',
     connection: {
-        host     : '127.0.0.1',
-        user     : 'mswanson',
-        // password : 'your_database_password',
-        database : 'chat',
-        charset  : 'utf8'
+        host     : 'ec2-204-236-226-63.compute-1.amazonaws.com',
+        user     : 'kvimdzxrahtabs',
+        password : '2zIgeT-Xrga02UMhubnquL5q2f',
+        database : 'd75kv4ska29qoq',
+        charset  : 'utf8', 
+        ssl		 : true
   }
 });
 var Bookshelf = require('bookshelf')(knex);
